4d768f1b622a7dfd83d82039b3b9a7e44e979016
mipotter-fnba <michael.potter@fnba.com>
2025-03-17 12:49:26 -0400

    [michael.potter DEVEX-710 chartool] changes for DEVEX-710

--- src/main/java/za/co/rin/chartool/generated/Charts.java
+++ src/main/java/za/co/rin/chartool/generated/Charts.java
@@ -70,7 +70,7 @@ public class Charts {
      */
     public List<Dashboard> getDashboard() {
         if (dashboard == null) {
-            dashboard = new ArrayList<Dashboard>();
+            dashboard = new ArrayList<>();
         }
         return this.dashboard;
     }
