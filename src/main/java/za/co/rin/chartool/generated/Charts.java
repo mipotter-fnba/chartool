5673abf25d9014f4f5568a2aba3d5d676f201003
mipotter-fnba <michael.potter@fnba.com>
2025-03-17 14:10:55 -0400

    [michael.potter DEVEX-710 chartool] changes for DEVEX-710

--- src/main/java/za/co/rin/chartool/generated/Charts.java
+++ src/main/java/za/co/rin/chartool/generated/Charts.java
@@ -10,11 +10,7 @@ package za.co.rin.chartool.generated;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.*;
 
 
 /**
